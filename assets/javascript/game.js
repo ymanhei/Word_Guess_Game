31ada603fdfffbcd7d7d91e183d8b473f104dcd9
Shing Chan <ymanhei@gmail.com>
2019-10-02 15:41:36 +0800

    it works1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,10 @@ var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var usedLetters = [];
 var correctLetter = [];
 var pn_array = computerGuess.split("");
-var failedguess = 10;
+var failedguess = 11;
 
 
 console.log("Name: " + computerGuess);
-
 /* var tmp_html = "";
 for (var i = 0; i < computerGuess.length; i++) {
     var tmp_html =  tmp_html + "<li>_</li>";    
@@ -24,11 +23,11 @@ function gen_alphabets_list(alphabets) {
     var alpha_list = "";
        for (var i = 0; i < alphabets.length; i++) { 
            if (usedLetters.indexOf(alphabets[i])==-1) {
-            alpha_list = alpha_list + "<li>"+alphabets[i]+"</li>";
+            alpha_list = alpha_list + "<li>"+alphabets[i]+"</li><span>  </span>";
            } 
            else {
             
-            alpha_list = alpha_list + "<li><del>"+alphabets[i]+"</del></li>";
+            alpha_list = alpha_list + "<li><del>"+alphabets[i]+"</del></li><span>  </span>";
            }   
         
        }     
@@ -44,11 +43,15 @@ document.onkeyup = function(event) {
       // Randomly chooses a choice from the options array. This is the Computer's guess.
      
 
-      document.getElementById('guess').innerHTML = userGuess;
-      usedLetters.push(userGuess);
-      gen_underscore(computerGuess,userGuess);   
-      gen_alphabets_list(alphabets);
+      document.getElementById('guess').innerHTML = "Your guess: " + userGuess;
+      
+      if (failedguess>0) {
+        gen_underscore(computerGuess,userGuess);   
+        gen_alphabets_list(alphabets);
 
+      }
+      
+      
 }
 
 function gen_underscore(playername,userGuess) {
@@ -58,22 +61,22 @@ function gen_underscore(playername,userGuess) {
  var tmpUS = "";
  var sucessfulguess = 0;
  
- console.log(failedguess);
+ //console.log(failedguess);
 
     for (var i = 0; i < pn_array.length; i++) {
         if ((pn_array[i]==userGuess) && (correctLetter.indexOf(userGuess)==-1)) {
-            tmpUS = tmpUS + "<li>" + userGuess + "</li><span> </span>";
+            tmpUS = tmpUS + "<li>" + userGuess + "</li><span>  </span>";
             correctLetter.push(userGuess);        
         }
     }
 
     for (var i = 0; i < pn_array.length; i++) { 
         if (correctLetter.indexOf(pn_array[i])==-1) {
-            var tmp_html = tmp_html + "<li>_</li>";
+            var tmp_html = tmp_html + "<li>_</li> <span>  </span>";
             
         }
         else {
-            var tmp_html =  tmp_html + "<li>" + pn_array[i] + "</li>";
+            var tmp_html =  tmp_html + "<li>" + pn_array[i] + "</li><span>  </span>";
             sucessfulguess++;
         }
  
@@ -83,15 +86,34 @@ function gen_underscore(playername,userGuess) {
 
     document.getElementById('underscore').innerHTML = us_html;
 
-    if (pn_array.indexOf(userGuess) ==-1 && failedguess > 0) {
-        document.getElementById('done').innerHTML =("You still have" + failedguess + "lives!");
-        failedguess--;
-    }
-    else if (failedguess == 0){
-        document.getElementById('done').innerHTML =("You still have no lives left. Game Over!");
+    if (pn_array.indexOf(userGuess) ==-1) {
+        
+/*         console.log("userGuess: " + userGuess);
+        console.log("usedletters: " + usedLetters);
+        console.log(usedLetters.indexOf(userGuess)); */
+        if (usedLetters.indexOf(userGuess)==-1) {         
+            failedguess--;
+            usedLetters.push(userGuess);
+            //console.log("failed: " + failedguess);           
+        }
+
+        /* if (usedLetters.indexOf(userGuess)==-1){
+            usedLetters.push(userGuess);
+          } */
+          if (failedguess > 0) {
+            document.getElementById('done').innerHTML =("You still have " + failedguess + "lives!");
+          }
+          else{
+            document.getElementById('done').innerHTML =("You still have no lives left. Game Over!");
+            return;
+          }
+                
     }
     else if (sucessfulguess == pn_array.length) {
 
         document.getElementById('done').innerHTML =("Well Done!");
+        console.log(computerGuess);
+        document.getElementById('pic').src = "assets/images/ronaldo.jpg";
+        return;
     }
   }
